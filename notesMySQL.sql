4f564f0f737f79e023fd2bd2ba5587c107a191ee
Anmol Verma <anmolverma102002@gmail.com>
2022-01-02 21:04:32 +0530

    SUM MySQL

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -570,3 +570,9 @@ SELECT author_fname,  author_lname,  Min(released_year)  FROM   books  GROUP  BY
 SELECT author_fname, author_lname, Max(pages) FROM books GROUP BY author_lname, author_fname;
 SELECT CONCAT(author_fname, ' ', author_lname) AS author, MAX(pages) AS 'longest book' 
     FROM books GROUP BY author_lname, author_fname;
+
+-- 🔴The Sum Function
+SELECT SUM(pages) FROM books;
+SELECT SUM(released_year) FROM books;
+SELECT author_fname, author_lname, Sum(pages) FROM books GROUP BY author_lname, author_fname;
+SELECT author_fname, author_lname, Sum(released_year) FROM books GROUP BY author_lname, author_fname;
