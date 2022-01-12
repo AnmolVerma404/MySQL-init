8915c38d7888a7fc9870564daa7e17c47455af79
Anmol Verma <anmolverma102002@gmail.com>
2022-01-12 13:14:22 +0530

    DATE & TIME datatype

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -623,3 +623,15 @@ INSERT INTO again_thingies(price) VALUES (8877665544.45);-- 8877665544.45
 SELECT * FROM again_thingies;
 INSERT INTO again_thingies(price) VALUES (8877665544.4572361872361);-- 8877665544.457235
 SELECT * FROM again_thingies;
+
+-- 🔴Creating Our DATE data
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+INSERT INTO people (name, birthdate, birthtime, birthdt) VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+INSERT INTO people (name, birthdate, birthtime, birthdt) VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+SELECT * FROM people;
+-- +-------+------------+-----------+---------------------+
+-- | name  | birthdate  | birthtime | birthdt             |
+-- +-------+------------+-----------+---------------------+
+-- | Padma | 1983-11-11 | 10:07:35  | 1983-11-11 10:07:35 |
+-- | Larry | 1943-12-25 | 04:10:42  | 1943-12-25 04:10:42 |
+-- +-------+------------+-----------+---------------------+
