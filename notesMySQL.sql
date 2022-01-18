2f0f35926ee04331f77bf13dc2436e96b280e619
Anmol Verma <anmolverma102002@gmail.com>
2022-01-18 18:06:46 +0530

    less than

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -721,3 +721,13 @@ SELECT 99 > 567;
 SELECT title, author_lname FROM books WHERE author_lname = 'Eggers';
 SELECT title, author_lname FROM books WHERE author_lname = 'eggers';
 SELECT title, author_lname FROM books WHERE author_lname = 'eGGers';
+
+-- 🔴Less Than
+SELECT title, released_year FROM books;
+SELECT title, released_year FROM books WHERE released_year < 2000;
+SELECT title, released_year FROM books WHERE released_year <= 2000;
+SELECT 3 < -10;    -- false
+SELECT -10 < -9;   -- true
+SELECT 42 <= 42;   -- true
+SELECT 'h' < 'p';  -- true
+SELECT 'Q' <= 'q'; -- true
