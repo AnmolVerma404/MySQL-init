931491f5a1650c13240477c73a070f7a7ea659e1
Anmol Verma <anmolverma102002@gmail.com>
2022-01-18 16:59:43 +0530

    Not Like

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -695,3 +695,10 @@ SELECT title FROM books WHERE released_year != 2017;
 SELECT title, author_lname FROM books;
 SELECT title, author_lname FROM books WHERE author_lname = 'Harris';
 SELECT title, author_lname FROM books WHERE author_lname != 'Harris';
+
+-- 🔴Not Like
+SELECT title FROM books WHERE title LIKE 'W';
+SELECT title FROM books WHERE title LIKE 'W%';
+SELECT title FROM books WHERE title LIKE '%W%';
+SELECT title FROM books WHERE title LIKE 'W%';
+SELECT title FROM books WHERE title NOT LIKE 'W%';
