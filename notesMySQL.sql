7d6eb019bd71daf884fddc469eb1876dad6ccf18
Anmol Verma <anmolverma102002@gmail.com>
2022-01-19 23:13:26 +0530

    Case Statements

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -780,3 +780,40 @@ SELECT title, released_year FROM books WHERE released_year NOT IN  (2000,2002,20
 SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year NOT IN  (2000,2002,2004,2006,2008,2010,2012,2014,2016);
 SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year % 2 != 0;
 SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year % 2 != 0 ORDER BY released_year;
+
+-- 🔴Case Statements
+SELECT title, released_year,
+       CASE 
+         WHEN released_year >= 2000 THEN 'Modern Lit'
+         ELSE '20th Century Lit'
+       END AS GENRE
+FROM books;
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+SELECT title,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+        ELSE '****'
+    END AS STOCK
+FROM books;
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
