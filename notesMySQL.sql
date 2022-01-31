3f02c9f35663a6f7d3824d24e654d39d81960664
Anmol Verma <anmolverma102002@gmail.com>
2022-01-31 16:15:38 +0530

    Identify most popular photo

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -1060,3 +1060,9 @@ SELECT *  FROM users ORDER BY created_at LIMIT 5;
 SELECT  DAYNAME(created_at) AS day, COUNT(*) AS total FROM users GROUP BY day ORDER BY total DESC LIMIT 2;
 -- Identify Inactive Users (users with no photos)
 SELECT username FROM users LEFT JOIN photos ON users.id = photos.user_id WHERE photos.id IS NULL;
+
+-- Identify most popular photo (and user who created it)
+SELECT  username, photos.id, photos.image_url,  COUNT(*) AS total FROM photos
+  INNER JOIN likes ON likes.photo_id = photos.id
+  INNER JOIN users ON photos.user_id = users.id
+  GROUP BY photos.id ORDER BY total DESC LIMIT 1;
