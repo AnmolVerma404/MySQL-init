5771e17f6ae135da27dc9a7e25d873e1b81c85a6
Anmol Verma <anmolverma102002@gmail.com>
2022-02-06 15:50:58 +0530

    Insta Database you can't follow yourself Trigger

--- notesMySQL.sql
+++ notesMySQL.sql
@@ -1142,3 +1142,18 @@ CREATE TRIGGER must_be_adult
     END;
 $$
 DELIMITER; 
+
+-- Insta Database you can't follow yourself Trigger
+DELIMITER $$
+CREATE TRIGGER prevent_self_follows
+    BEFORE INSERT ON follows FOR EACH ROW 
+    BEGIN 
+        IF NEW.follower_id = NEW.followee_id
+        THEN 
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = "You cannot follow yourself!";
+        END IF;
+    END;
+$$
+DELIMITER ;
+
